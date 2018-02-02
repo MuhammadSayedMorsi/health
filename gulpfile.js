415328925d306a6a816718788d4df4490ccd630d
muhammad sayed <muhammadsayed1001@yahoo.com>
2018-02-02 04:14:17 +0200

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ const gulp        = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass        = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const image = require('gulp-image');
+
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
     return gulp.src(['assets/css/scss/**/*.scss'])
 
-    .pipe(sass({outputStyle: ''}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
       cascade: false
@@ -16,6 +18,11 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
+gulp.task('image', function () {
+    return gulp.src('assets/img/**')
+        .pipe(image())
+        .pipe(gulp.dest('assets/img'));
+});
 
 // Watch Sass & Serve
 gulp.task('serve', ['sass'], function() {
@@ -31,4 +38,4 @@ gulp.watch("assets/js/*.js").on('change', browserSync.reload);
 });
 
 // Default Task
-gulp.task('default', ['serve']);
+gulp.task('default', ['serve', 'image']);
